905beba3db1d02932e6d8045430e5a3b310ed527
emil.abbas <emil.abbas@procure.az>
2023-10-20 00:32:52 +0400

    main

---
+++ DataStructure/ArrayExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+
+namespace DataStructure;
+
+public class ArrayExample<T>
+{
+    public ArrayExample()
+    {
+    }
+
+    T[] Add(T[] array, T item, int index)
+    {
+        if (array.Length < index - 1)
+        {
+            T[] copy = new T[index - 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                copy[i] = array[i];
+            }
+
+            copy[index] = item;
+            array = copy;
+            return array;
+        }
+
+        array[index] = item;
+        return array;
+    }
+    
+    
+}
---
+++ DataStructure/ArrayListExample.cs
@@ -0,0 +1,6 @@
+﻿namespace DataStructure;
+
+public class ArrayListExample
+{
+    
+}
